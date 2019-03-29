13b39d4955c583faa04f4979a7daa8b68fe5225e
Petr Broz <petr.broz@autodesk.com>
2019-03-29 11:19:44 +0100

    Added check for required env. vars.

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require('path');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
-if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET) {
-    console.warn('Provide FORGE_CLIENT_ID and FORGE_CLIENT_SECRET env. variables to run this application.');
+const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_MODEL_URN, MONGODB_URL } = process.env;
+if (!FORGE_CLIENT_ID || !FORGE_CLIENT_SECRET || !FORGE_MODEL_URN || !MONGODB_URL) {
+    console.warn('Provide all the following env. variables to run this application:');
+    console.warn('FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_MODEL_URN, MONGODB_URL');
     return;
 }
 
